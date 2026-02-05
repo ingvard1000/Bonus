c2f8dae1e82fcff37a0fed24d96cd1971fa812e3
Ingvard <ingvard777333@gmail.com>
2026-02-05 16:03:34 +0300

    RenamePercentToPercent

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -7,15 +7,12 @@ public class BonusServiceTest {
     void shouldCalculateForRegisteredAndUnderLimit() {
         BonusService service = new BonusService();
 
-        // подготавливаем данные:
         long amount = 1000;
         boolean registered = true;
         long expected = 30;
 
-        // вызываем целевой метод:
         long actual = service.calculate(amount, registered);
 
-        // производим проверку (сравниваем ожидаемый и фактический):
         Assertions.assertEquals(expected, actual);
     }
 
@@ -23,15 +20,13 @@ public class BonusServiceTest {
     void shouldCalculateForRegisteredAndOverLimit() {
         BonusService service = new BonusService();
 
-        // подготавливаем данные:
         long amount = 1_000_000;
         boolean registered = true;
         long expected = 500;
 
-        // вызываем целевой метод:
         long actual = service.calculate(amount, registered);
 
-        // производим проверку (сравниваем ожидаемый и фактический):
+
         Assertions.assertEquals(expected, actual);
     }
 
@@ -39,7 +34,6 @@ public class BonusServiceTest {
     void shouldCalculateForUnregisteredAndOverLimit() {
         BonusService service = new BonusService();
 
-        // подготавливаем данные:
         long amount = 1000;
         boolean registered = false;
         long expected = 10;
